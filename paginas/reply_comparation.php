b641cf8866cfd80a95b7562f8a438f511f636a51
AlvaroRGZ <alu0101362953@ull.edu.es>
2023-03-21 11:23:39 +0000

    Added passwd to request

--- paginas/reply_comparation.php
+++ paginas/reply_comparation.php
@@ -3,38 +3,38 @@
 <!DOCTYPE html>
 <link rel="stylesheet" href="./css/content.css">
 <html>
-
-
   <body>
-
-
-
-
     <?php
       if($_SERVER['REQUEST_METHOD'] == 'GET') {
         // Retrieve data from URI
-        // Request de la forma: uid=X&locker=Y
-        //                      \-4-/ \--7--/
+        // Request de la forma: uid=X&psswd=Y&locker=Z
+        //                      \-4-/ \--6-/  \--7--/
         $uidLen = 4;
+        $psswdLen = 6;
         $lockerLen = 7;
         $data = $_SERVER["QUERY_STRING"];
         $uid = substr($data, $uidLen, stripos($data, "&") - 1);
-        $locker = substr($data, stripos($data, "&") + 1 + $uidLen, strlen($data));
+        $data = substr(stripos($data, "&") + 1, strlen($data));
+        $passwd = substr($data, stripos($data, "&") + 1, strlen($data));
         $db = new SQLite3('../bbdd/acceso_makerspace.db');
         // Selecciona el nombre del usuario que tenga permiso en ese armario con esa tarjeta
         $query = "SELECT nombre FROM usuarios natural join permisos WHERE armario_1 = 1 and id_usuario = '$uid';";
+        echo "uid = '$uid'";
+        echo "passwd = '$passwd'";
+        echo "locker = '$locker'";
+        /*
         $resultado = $db->query($query);
         if (!$resultado) {die($db->lastErrorMsg());} else {
           $user = $resultado->fetchArray();
         }
-
+        */
         $db->close();
         // Envia el resultado al cliente
         // joson_encode(array('usuario'=>$user));
         // devolver la respuesta al cliente
         header('Content-Type: text/plain');
         header('HTTP/1.1 200 OK');
-        echo "Alvaro";
+        // echo resultado;
       }
     ?>
   </body>
